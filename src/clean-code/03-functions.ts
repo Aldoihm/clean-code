36cf1312e99f51dba0f55646d8633b8fd1f67f83
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2026-01-06 10:14:26 -0600

    Nombres en funciones

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,37 @@
+
+(() => {
+
+    function getMovieById( id: string ) {
+        console.log({ id });
+    }
+
+    function getMovieCastById( id: string ) {
+        console.log({ id });
+    }
+    function getActorBioById( id: string ) {
+        console.log({ id });
+    }
+    
+    interface Movie{
+        title: string;
+        description: string;
+        rating: number;
+        cast: string[];
+    }
+    function createMovie({title, description, rating, cast}: Movie ) {
+        console.log({ title, description, rating, cast });
+    }
+
+    function checkifActorExists(fullName: string): boolean {
+        console.log({fullName})
+        return false;    
+    }
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        if (!checkifActorExists(fullName)){
+            console.log('Crear actor');
+            return true;
+        }else
+            return false;        
+    }
+
+})();
