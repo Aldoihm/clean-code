dc9dd1df73aa4ac7944467d0053c53a9960f7b45
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2026-01-15 09:13:25 -0600

    Aplicando un ejemplo de dont repeat yourself, solo fue un ejemplo, porque aún se puede refactorizar el código

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -9,18 +9,38 @@ class Product{
 
     ){}
 
-    toString(){
+    isProductReady():boolean{
         // No Dry = Don't Repeat Yourself. Hacemos 3 validaciones y se está repitiendo el código
+        /* Con esto inicia el ejemplo
         if(this.name.length<=0) throw Error('name is empty')
         if(this.price<=0) throw Error('price is zero')
         if(this.size.length<=0) throw Error('size is empty')
-        
+        */
+        /** this, hace referencia a la clase, pero en sí es un array de propiedades de la clase, y keys, son las propiedades del constructor */
+        for(const key in this){
+            switch (typeof this[key]){
+                case 'string':
+                    if(this[key].toString().length<=0) throw Error(`${ key } is empty`)
+                break;
+                case 'number':
+                    if(this[key].valueOf()<=0) throw Error(`${key} is zero`)
+                break;
+                default:
+                    throw Error(`${ typeof this[key] } is not valid`)
+            }
+        }
+
+        return true;
+    }
+
+    toString(){
+        if (!this.isProductReady) return;
 
         return `${this.name} (${ this.price}), ${ this.size}`
     }
 }
 
 (()=>{
-    const bluePants = new Product('Blue large pants',10,'XL');
+    const bluePants = new Product('Blue large pants',5,'XL');
     console.log(bluePants.toString())
 })();
