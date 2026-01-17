a992d152ae0c793958907f0e0049d218c634835d
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2026-01-17 09:30:00 -0600

    Rompiendo el principio de responsabilidad única en Herencia, ya que cada clase solo debería hacer una cosa, y en herencia es imposible seguir este principio

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,32 +1,60 @@
 (()=>{
 
-type Gender = 'M'|'F';    
+    
+    type Gender = 'M'|'F';    
 
-class Person{
-    /** 
-     * Forma que conozco y larga de manejar clases 
-    public name: string;
-    public gender: Gender;
-    public birthdate: Date;
-
-    constructor(name:string, gender:Gender, birtdate: Date){
-        this.name = name;
-        this.gender = gender;
-        this.birthdate = birtdate;
+    class Person{
+        constructor(
+            public name     : string,
+            public gender   : Gender,
+            public birthdate: Date
+        ){}
     }
-    */
 
-    /** Forma Corta y nueva forma que voy a adquirir */
-    constructor(
-        public name:string,
-        public gender:Gender,
-        public birthdate:Date
-    ){}
+    class User extends Person {
+        public lastAccess: Date;
+        
+        constructor(
+            name                : string,
+            gender              : Gender,
+            birthdate           : Date,
+            public email        : string,
+            public role         : string,
+        ){
+            super(name,gender,birthdate);
+            this.lastAccess = new Date();
+        }
 
+        checkCredentials(){
+            return true;
+        }
+    }
 
-}
+    //UserSettings no aplicando el principio de responsabilidad única.
+    class UserSetting extends User{
+        constructor(
+            public workingDirectory : string,
+            public lastOpenFolder   : string,
+            email                   : string,
+            role                    : string,
+            name                    : string,
+            gender                  : Gender,
+            birthdate               : Date
+        ){
+            super(name,gender,birthdate,email,role);
+        }
+    }
 
-const newPerson = new Person('Fernando','M',new Date('1985-10-21'));
-console.log( {newPerson} );
+    const userSetttings = new UserSetting(
+        '/usr/home',
+        '/home',
+        'aldoihm@gmail.com',
+        'Admin',
+        'Aldo',
+        'M',
+        new Date('1989-12-10')
+    );
+
+    console.log({ userSetttings});
 
 })();
