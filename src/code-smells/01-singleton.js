8e9758965f89bcbe07ad66cc0a69891bd195fe65
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2026-01-25 22:35:56 -0600

    Habla de singleton, aunque aquí no lo recomienda, en angular lo usan al parecer para conectar con otras aplicaciones

---
+++ src/code-smells/01-singleton.js
@@ -0,0 +1,35 @@
+/**
+ * Singleton
+ * Está en un ambito global 
+ * No está adentro de ninguna función.
+ * No está dentro de alguna clase
+ * Está en donde la computadora, lo quiera colocar en la memoria ram
+ */
+
+const Singleton = (function () {
+    
+    let instance;
+
+    function createInstance() {
+        return new Object('I am the instance');
+    }
+    //al parecer lo que está en return es como su método público
+    return {
+        getInstance() {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
+
+function main() {
+
+    const instance1 = Singleton.getInstance();
+    const instance2 = Singleton.getInstance();
+
+    console.log('Misma instancia? ', (instance1 === instance2));
+}
+
+main();
